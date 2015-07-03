699dafbddaa35a275d15a4747a6b1e5c0e249fa9
Michał Dudek <michal@michaldudek.pl>
2015-07-03 14:46:09 +0200

    Detect env based on .tld.

--- web/app.php
+++ web/app.php
@@ -4,4 +4,9 @@ require_once __DIR__ .'/../vendor/autoload.php';
 use Splot\Framework\Framework;
 use Application\App;
 
-Framework::run(new App(), 'prod', true);
+// detect env based on .tld
+$tld = end(explode('.', $_SERVER['SERVER_NAME']));
+$env = $tld === 'dev' ? 'dev' : 'prod';
+$debug = $env === 'dev' ? true : false;
+
+Framework::run(new App(), $env, $debug);
