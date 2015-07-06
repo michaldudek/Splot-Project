1944d705f709ede4c459a8838e52f7785279f55a
Michał Dudek <michal@michaldudek.pl>
2015-07-06 02:33:51 +0200

    We don't need SplotAssetsModule anymore.

--- src/Application/App.php
+++ src/Application/App.php
@@ -17,7 +17,6 @@ class App extends AbstractApplication
             new \Splot\FrameworkExtraModule\SplotFrameworkExtraModule(),
             new \Splot\KnitModule\SplotKnitModule(),
             new \Splot\TwigModule\SplotTwigModule(),
-            new \Splot\AssetsModule\SplotAssetsModule(),
 
             new \MD\Placeholder\MDPlaceholderModule()
         ];
